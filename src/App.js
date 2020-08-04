d0e2b46c0f589fd4d370ab8c3bd7fcecbb956101
nasrullah013 <dani.nasrullah1998@gmail.com>
2020-08-04 22:38:15 +0700

    Update Tugas Hari Ke-2 Pekan 3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import DaftarBuah from './tugas11/DaftarBuah'
+import Clock from './tugas12/Clock'
 
 function App() {
   return (
     <div>
       {/* Tugas11 */}
         <DaftarBuah />
+        {/* Tugas12 */}
+        <Clock start={100}/>
     </div>
   )
 }
---
+++ src/tugas12/Clock.js
@@ -0,0 +1,54 @@
+import React, {Component} from 'react'
+
+class Clock extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            date: new Date(),
+            time: 10
+        }
+    }
+
+    componentDidMount(){
+        if (this.props.start !== undefined){
+            this.setState({time: this.props.start})
+        }
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        )
+    }
+
+    componentDidUpdate() {
+        if (this.state.time < 0) {
+            this.componentWillUnmount()
+        }
+    }
+
+    componentWillUnmount(){
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        this.setState({
+            date: new Date(),
+            time: this.state.time - 1
+        });
+    }
+
+    render(){
+        return(
+        <>
+            {
+            this.state.time > 0 &&
+                <div style={{width:"800px", margin:"auto"}}>
+                    <h2 style={{float: "left"}}>Sekarang jam : {this.state.date.toLocaleTimeString()}</h2>
+                    <h2 style={{float: "right"}}>Hitungan mundur : {this.state.time}</h2>
+                </div>
+            }
+        </>
+        )
+    }
+}
+
+export default Clock
