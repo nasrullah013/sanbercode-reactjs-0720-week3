aaac635afe69a4363a1b1031143cdc6378ae1a98
nasrullah013 <dani.nasrullah1998@gmail.com>
2020-08-06 00:32:20 +0700

    Update Tugas Hari Ke-3 Pekan 3

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './App.css';
-import DaftarBuah from './tugas11/DaftarBuah'
 import Clock from './tugas12/Clock'
+import DaftarBuah from './tugas13/DaftarBuah'
 
 function App() {
   return (
     <div>
       {/* Tugas11 */}
-        <DaftarBuah />
+        {/* <DaftarBuah /> */}
         {/* Tugas12 */}
         <Clock start={100}/>
+        {/* tugas13 */}
+        <DaftarBuah />
     </div>
   )
 }
